aec00e5486a57295522c6018a1f7ed48e7b1338b
sungwon2598 <tjddnjs2598@gmail.com>
2023-10-19 09:05:07 +0900

    docs : 이게제목이고
    이게 본문?

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -4,5 +4,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
         System.out.println("커밋 연습");
+        System.out.println("커밋 메시지 연습");
     }
 }
